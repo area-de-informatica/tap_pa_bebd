2a324ed3168348830963faca2ec1d8adb949c98f
ymorelochavez49 <ymorelochavez49@correo.unicordoba.edu.co>
2025-05-12 03:16:11 +0000

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+    <SurveyGroup/>
+    </div>
+  <div>
+  <Surveyform/>
+  </div>
+</template>
+
+<script setup>
+import SurveyGroup from '~/components/SurveyGroup.vue'
+import Surveyform from '~/components/Surveyform.vue'
+</script>
